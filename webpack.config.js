5d4b0cd8cfce982590f048afcc76f97c2268f75d
mizuki-yamada <my79909369@gmail.com>
2023-08-27 18:12:10 +0900

    JSXで要素を表示、ライブリロードを使えるようにした

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,34 @@ const path = require("path");
 
 module.exports = {
   mode: process.env.NODE_ENV || "development",
-  entry: "./app.js",
+  entry: "./src/app.tsx",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-};
-
-resolve: {
-  extensions: [".js", ".ts", ".tsx"];
-}
-
-module: {
-  rules: [
-    {
-      test: /\.(j|t)sx?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel_loader",
-        options: {
-          presets: [
-            "@babel/preset-env",
-            "@babel/preset-react",
-            "@babel/preset-typescript",
-          ],
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
         },
       },
-    },
-  ];
-}
+    ],
+  },
+  devServer: {
+    contentBase: __dirname,
+    port: 5000,
+  },
+};
